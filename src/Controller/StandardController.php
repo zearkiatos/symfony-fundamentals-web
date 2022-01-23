c9afa80c35428c5ffb7e3e9cc734bec96401fb39
zearkiatos <caprilespe@outlook.com>
2022-01-23 13:07:38 -0300

    A look to twig

--- src/Controller/StandardController.php
+++ src/Controller/StandardController.php
@@ -22,15 +22,28 @@ class StandardController extends AbstractController
         return $this->render('standard/page.html.twig');
     }
 
+    #[Route('/page/{param1}/{param2}', name: 'page-with-param')]
+    public function pageWithParam($param1, $param2)
+    {
+        $result = $param1 * $param2;
+        return $this->render('standard/page.html.twig', [
+            'param1' => $param1,
+            'param2' => $param2,
+            'result' => $result
+        ]);
+    }
+
     #[Route('/redirect-to-page', name: 'redirect-to-page')]
-    public function redirectToPage() {
+    public function redirectToPage()
+    {
         return $this->redirectToRoute('page');
     }
 
     #[Route('/redirect-to-page-with-param/{id}/{name}', name: 'redirect-to-page-with-param')]
-    public function redirectToPageWithParam($id, $name) {
+    public function redirectToPageWithParam($id, $name)
+    {
         dump($id);
         dump($name);
-        return $this->redirectToRoute('page', ['id' => $id, 'name' => $name]);
+        return $this->redirectToRoute('page-with-param', ['param1' => $id, 'param2' => $name]);
     }
 }
